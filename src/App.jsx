630e088a5702cc82a623941bfb8d031e97b319ab
JorgeAntonioJr <jorgeantonio101001@gmail.com>
2023-05-23 19:56:02 -0300

    Primeiro Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
       </header>
       <div className="lista-compras-container">
         <ul className="lista-items">
-          <li>Queijo</li>
-          <li>Leite</li>
-          <li>Pão</li>
+
         </ul>
       </div>
       <form className="form-add-item" action="#" method="post">
